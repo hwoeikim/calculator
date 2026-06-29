1a5308ce273ec401df7096502ca21dab57fafb9a
hwoeikim <hwoeikim@gmail.com>
2026-06-29 16:29:28 +0900

    level 2-2. App 클래스에서 Calculator 클래스 활용

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,19 +1,16 @@
 package com;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-record Result(int result) {}
 
 public class App {
     public static void main(String[] args) {
 
-        List<Result> results = new ArrayList<>();
+        Calculator calculator = new Calculator();
 
         Scanner sc = new Scanner(System.in);
 
-        while (true) {// 양의 정수 2개 전달 받기
+        while (true) {
 
             System.out.println("첫 번째 숫자를 입력하세요: ");
             int a = readInt(sc);
@@ -21,62 +18,34 @@ public class App {
             System.out.println("두 번째 숫자를 입력하세요: ");
             int b = readInt(sc);
 
-            // 사칙연산 기호 전달 받기
-
             System.out.println("사칙연산 기호(+, -, *, /)를 입력하세요: ");
             char operator = readOperator(sc);
 
-            // 연산 진행 및 결과값 출력
+            // 입력받은 값 연산 수행 역할은 Calculator 클래스가 담당
 
-            int result = 0;
-
-            switch (operator) {
-                case '+':
-                    result = a + b;
-                    break;
-                case '-':
-                    result = a - b;
-                    break;
-                case '*':
-                    result = a * b;
-                    break;
-                case '/':
-                    if (b == 0) {
-                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                        continue;
-                    }
-                    result = a / b;
-                    break;
+            try {
+                int result = calculator.calculate(a, b, operator);
+                System.out.println("결과: " + result);
+            } catch (CalculatorException e) {
+                System.out.println("Error: " + e.getMessage());
+                continue;
             }
 
-            System.out.println("결과: " + result);
-
-            // 결과를 저장(무한)
-            results.add(new Result(result));
 
             // 가장 먼저 저장된 결과 삭제
-            // (여기선 값이 없을 일은 없지만 연습용 에러방지)
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
 
             String answerRemove = sc.next();
             if (answerRemove.equals("remove")) {
-                if(!results.isEmpty()) {
-                    results.remove(0);
-                    System.out.println("성공적으로 삭제되었습니다.");
-                } else {
-                    System.out.println("삭제할 기록이 없습니다.");
-                }
+                calculator.removeResult();
             }
 
             // 저장된 연산 결과 모두 출력
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회");
+            System.out.println("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
 
             String answerInquiry = sc.next();
             if (answerInquiry.equals("inquiry")) {
-                for (Result r : results) {
-                    System.out.println(r.result());
-                }
+                calculator.inquiryResults();
             }
 
             // 종료 여부
--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -3,6 +3,9 @@ package com;
 import java.util.ArrayList;
 import java.util.List;
 
+// 구성을 꺼내는 메서드 자동 생성
+record Result(int result) {}
+
 public class Calculator {
     private List<Result> results = new ArrayList<>();
 
@@ -18,6 +21,7 @@ public class Calculator {
         if (operator == '/' && b == 0) {
             throw new CalculatorException("나눗셈에서 두번째 숫자(분모)는 0일 수 없습니다.");
         }
+
 
         int calculatorResult = 0;
 
@@ -39,5 +43,36 @@ public class Calculator {
         results.add(new Result(calculatorResult));
         return calculatorResult;
     }
+
+    public void removeResult() {
+        if(!results.isEmpty()) {
+            results.remove(0);
+            System.out.println("성공적으로 삭제되었습니다.");
+        } else {
+            System.out.println("삭제할 기록이 없습니다.");
+        }
+    }
+
+
+    public List<Result> getResults() {
+        return this.results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public void inquiryResults() {
+        if (results.isEmpty()) {
+            System.out.println("조회할 기록이 없습니다.");
+            return;
+        }
+        for (Result r : results) {
+            System.out.println("연산결과 목록: " + r.result());
+        }
+
+    }
+
+
 
 }
