26542e66d67a54540c162352817bcd5d687a991d
hwoeikim <hwoeikim@gmail.com>
2026-06-29 14:37:38 +0900

    level 2-1. Calculator 클래스 생성

---
+++ src/main/java/com/Calculator.java
@@ -0,0 +1,43 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    private List<Result> results = new ArrayList<>();
+
+    public Calculator () {
+
+    }
+
+    public int calculate(int a, int b, char operator) {
+
+        if (a < 0 || b < 0) {
+            throw new CalculatorException("0 이상의 양의 정수를 입력해야 합니다.");
+        }
+        if (operator == '/' && b == 0) {
+            throw new CalculatorException("나눗셈에서 두번째 숫자(분모)는 0일 수 없습니다.");
+        }
+
+        int calculatorResult = 0;
+
+        switch (operator) {
+            case '+':
+                calculatorResult = a + b;
+                break;
+            case '-':
+                calculatorResult = a - b;
+                break;
+            case '*':
+                calculatorResult = a * b;
+                break;
+            case '/':
+                calculatorResult = a / b;
+                break;
+        }
+
+        results.add(new Result(calculatorResult));
+        return calculatorResult;
+    }
+
+}
---
+++ src/main/java/com/CalculatorException.java
@@ -0,0 +1,8 @@
+package com;
+
+public class CalculatorException extends RuntimeException {
+
+    public CalculatorException(String message) {
+        super(message);
+    }
+}
